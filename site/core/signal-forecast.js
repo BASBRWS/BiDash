b08f4e23d47498bbcc09f2d14aee60e82f617ac8
ETAKS <sebastiaan.Boom@gmail.com>
2026-09-14 14:46:29 +0200

    Activeer automatische live filter synchronisatie

--- site/core/signal-forecast.js
+++ site/core/signal-forecast.js
@@ -1,7 +1,9 @@
 import {installDvmLiveSnapshotPatch} from './live-snapshot.js';
 import {installDvmLiveOverviewFilterPatch} from './live-overview-filter.js';
+import {installLiveOverviewFilterSync} from './live-overview-filter-sync.js';
 export * from './signal-forecast-original.js';
 export * from './live-filter.js';
 
 installDvmLiveSnapshotPatch(globalThis);
 installDvmLiveOverviewFilterPatch(globalThis);
+installLiveOverviewFilterSync(globalThis);
