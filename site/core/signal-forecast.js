ebf4e2496df916dbeb78a196c2104556a9c52c4a
ETAKS <sebastiaan.Boom@gmail.com>
2026-09-14 15:08:36 +0200

    Toon DVM-bronvoortgang in de hoofdapp

--- site/core/signal-forecast.js
+++ site/core/signal-forecast.js
@@ -1,9 +1,11 @@
 import {installDvmLiveSnapshotPatch} from './live-snapshot.js';
 import {installDvmLiveOverviewFilterPatch} from './live-overview-filter.js';
 import {installLiveOverviewFilterSync} from './live-overview-filter-sync.js';
+import {installDvmImportProgressBridge} from './import-progress-bridge.js';
 export * from './signal-forecast-original.js';
 export * from './live-filter.js';
 
 installDvmLiveSnapshotPatch(globalThis);
 installDvmLiveOverviewFilterPatch(globalThis);
 installLiveOverviewFilterSync(globalThis);
+installDvmImportProgressBridge(globalThis);
