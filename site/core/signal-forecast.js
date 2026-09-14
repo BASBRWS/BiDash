52dc99a0159f5f207d2fcbca837a0cc2f6d69a06
ETAKS <sebastiaan.Boom@gmail.com>
2026-09-14 14:16:08 +0200

    Activeer live storingsfilter in DVM runtime

--- site/core/signal-forecast.js
+++ site/core/signal-forecast.js
@@ -1,4 +1,7 @@
 import {installDvmLiveSnapshotPatch} from './live-snapshot.js';
+import {installDvmLiveFilterPatch} from './live-filter.js';
 export * from './signal-forecast-original.js';
+export * from './live-filter.js';
 
 installDvmLiveSnapshotPatch(globalThis);
+installDvmLiveFilterPatch(globalThis);
