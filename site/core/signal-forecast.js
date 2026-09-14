0a9794f51e0372b8903423e80021f6d825cc5e31
ETAKS <sebastiaan.Boom@gmail.com>
2026-09-14 15:45:06 +0200

    Herstel signal-forecast na diagnosecorrectie

--- site/core/signal-forecast.js
+++ site/core/signal-forecast.js
@@ -2,7 +2,6 @@ import {installDvmLiveSnapshotPatch} from './live-snapshot.js';
 import {installDvmLiveOverviewFilterPatch} from './live-overview-filter.js';
 import {installLiveOverviewFilterSync} from './live-overview-filter-sync.js';
 import {installDvmImportProgressBridge} from './import-progress-bridge.js';
-import {installDvmImportFastCapture} from './dvm-import-fast-capture.js';
 export * from './signal-forecast-original.js';
 export * from './live-filter.js';
 
@@ -10,4 +9,3 @@ installDvmLiveSnapshotPatch(globalThis);
 installDvmLiveOverviewFilterPatch(globalThis);
 installLiveOverviewFilterSync(globalThis);
 installDvmImportProgressBridge(globalThis);
-installDvmImportFastCapture(globalThis);
