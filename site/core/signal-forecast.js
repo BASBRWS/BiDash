b6d1782171c8a4362f7ec400cd08aa43699469f4
ETAKS <sebastiaan.Boom@gmail.com>
2026-09-14 16:55:59 +0200

    Activeer importoptimalisatie voor analysebeeld

--- site/core/signal-forecast.js
+++ site/core/signal-forecast.js
@@ -2,6 +2,7 @@ import {installDvmLiveSnapshotPatch} from './live-snapshot.js';
 import {installDvmLiveOverviewFilterPatch} from './live-overview-filter.js';
 import {installLiveOverviewFilterSync} from './live-overview-filter-sync.js';
 import {installDvmImportProgressBridge} from './import-progress-bridge.js';
+import {installDvmAnalysisRebuildPerformance} from './dvm-analysis-rebuild-performance.js';
 export * from './signal-forecast-original.js';
 export * from './live-filter.js';
 
@@ -9,3 +10,4 @@ installDvmLiveSnapshotPatch(globalThis);
 installDvmLiveOverviewFilterPatch(globalThis);
 installLiveOverviewFilterSync(globalThis);
 installDvmImportProgressBridge(globalThis);
+installDvmAnalysisRebuildPerformance(globalThis);
