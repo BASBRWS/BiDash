f922db75bc78afa817445b4b8dcf5391caeb5a73
ETAKS <sebastiaan.Boom@gmail.com>
2026-09-14 15:34:24 +0200

    Activeer snelle capture na DVM import

--- site/core/signal-forecast.js
+++ site/core/signal-forecast.js
@@ -2,6 +2,7 @@ import {installDvmLiveSnapshotPatch} from './live-snapshot.js';
 import {installDvmLiveOverviewFilterPatch} from './live-overview-filter.js';
 import {installLiveOverviewFilterSync} from './live-overview-filter-sync.js';
 import {installDvmImportProgressBridge} from './import-progress-bridge.js';
+import {installDvmImportFastCapture} from './dvm-import-fast-capture.js';
 export * from './signal-forecast-original.js';
 export * from './live-filter.js';
 
@@ -9,3 +10,4 @@ installDvmLiveSnapshotPatch(globalThis);
 installDvmLiveOverviewFilterPatch(globalThis);
 installLiveOverviewFilterSync(globalThis);
 installDvmImportProgressBridge(globalThis);
+installDvmImportFastCapture(globalThis);
