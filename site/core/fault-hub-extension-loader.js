6fee897402fa128804cb84c53a63f3c4754eb3e3
ETAKS <sebastiaan.Boom@gmail.com>
2026-09-15 14:06:47 +0200

    Synchroniseer open-storingen UI na HUB-verrijking

--- site/core/fault-hub-extension-loader.js
+++ site/core/fault-hub-extension-loader.js
@@ -5,6 +5,7 @@ export function installFaultHubExtensionLoader(scope=globalThis){
     if(scope.document.getElementById('bidashFaultsExtensionScript'))return;
     const s=scope.document.createElement('script');
     s.id='bidashFaultsExtensionScript';s.src='dvm-faults-extension.js';s.defer=true;
+    s.onload=()=>scope.dispatchEvent(new CustomEvent('bidash:faults-extension-ready'));
     scope.document.head.appendChild(s);
   };
   if(scope.document.readyState==='complete')run();else scope.addEventListener('load',run,{once:true});
