2deb41a4e55671572bfce742ebe3bd859317bf78
ETAKS <sebastiaan.Boom@gmail.com>
2026-09-19 16:41:18 +0200

    Verhoog BiDash versie naar 2.19

--- tests/drip-special-lists.test.js
+++ tests/drip-special-lists.test.js
@@ -70,6 +70,6 @@ test('open storingen gebruikt zowel assetmarkering als referentielijst',()=>{
 });
 
 test('zichtbare versie is verhoogd',()=>{
-  assert.match(read('site/core/versie.js'),/BIDASH_VERSIE='2\.18'/);
+  assert.match(read('site/core/versie.js'),/BIDASH_VERSIE='2\.19'/);
   assert.match(manager,/DVM_VERSION='102'/);
 });
